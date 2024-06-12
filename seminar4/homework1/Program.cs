85b57da7511b7a67298ec5847b557792f5008ad9
«TanueSha» <tvryabova@gmail.com>
2024-06-12 19:38:40 +0300

    homework1

---
+++ seminar4/homework1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 1: Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
+// Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+
+    while(true)
+    {
+        Console.WriteLine("Введите целое число:");
+        string input = Console.ReadLine();
+        {
+            Console.WriteLine("Для остановки цикла введите 'q'");
+            if (input == "q")
+            {
+                break;
+            }
+            else
+            {
+                int number = Convert.ToInt32(input);
+                int sum = 0;
+                while (number > 0)
+                {
+                    sum += number % 10;
+                    number /= 10;
+                }
+                if (sum % 2 == 0)
+                {
+                    Console.WriteLine("Сумма цифр чётная");
+                }
+                
+            }
+           
+        }
+        
+    }
+
+
