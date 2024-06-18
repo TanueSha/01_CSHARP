1b9527554e9d8d5c60b88c5ff169895322f3f771
«TanueSha» <tvryabova@gmail.com>
2024-06-18 19:50:59 +0300

    seminar6:homework

---
+++ seminar6/homework2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. 
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+string message = string.Empty;
+
+string LowercaseLetters(string message)
+{
+      
+    
+    return message.ToLower();
+    
+}
+
+Console.WriteLine("Введите строку из латинских букв в нижнем и верхнем регистре:");
+string str = Console.ReadLine();
+Console.WriteLine(LowercaseLetters(str));
