f18e3a7fdb1af9b88a1b3184863570956236a9a8
«TanueSha» <tvryabova@gmail.com>
2024-06-19 22:37:51 +0300

    homework4

---
+++ seminar6/homework4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 4*(не обязательная): Задайте строку, состоящую из слов, разделенных пробелами. 
+// Сформировать строку, в которой слова расположены в обратном порядке. 
+// В полученной строке слова должны быть также разделены пробелами.
+
+string message = "Вова Таня Саша Катя";
+string[] words = message.Split(' ');
+for (int i = 0; i < words.Length / 2; i++)
+{
+    string temp = words[i];
+    words[i] = words[words.Length - i -1];
+    words[words.Length - i -1] = temp;
+
+    
+}
+for (int i = 0; i < words.Length; i++)
+{
+    Console.Write(words[i] + " ");
+}
