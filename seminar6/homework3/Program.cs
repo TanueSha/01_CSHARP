cf615f4ed13188947655bf14cb0d5a15a8b35547
«TanueSha» <tvryabova@gmail.com>
2024-06-19 21:34:43 +0300

    homework3

---
+++ seminar6/homework3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+string message = "а роза упала на руку Азора";
+
+
+bool IsPalindrome(string message)
+{
+    message = message.Replace(" ", "").ToLower();
+
+    for (int i = 0; i < message.Length / 2; i++)
+    {
+        if (message[i] != message[message.Length - i - 1])
+        {
+            Console.WriteLine("Строка не является палиндромом");
+            return false;
+        }
+       
+    }
+    System.Console.WriteLine("Строка является палиндромом");
+    return true;
+    
+}
+
+
+System.Console.WriteLine(IsPalindrome(message));
+
