510b407f3aca2e9d1abae892b696c2df252ac1dc
«TanueSha» <tvryabova@gmail.com>
2024-06-21 23:10:41 +0300

    HOMEWORK:задача2

--- seminar7/HOMEWORK/Program.cs
+++ seminar7/HOMEWORK/Program.cs
@@ -16,3 +16,37 @@ int MToN(int m, int n)
 
 System.Console.WriteLine(MToN(1, 5));
 
+// Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+
+
+Console.Write("Введите значение m: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите значение n: ");
+int n = int.Parse(Console.ReadLine());
+
+
+int result = Ackermann(m, n);
+Console.WriteLine($"Ackermann({m}, {n}) = {result}");
+
+
+// Рекурсивная функция Аккермана
+int Ackermann(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0)
+    {
+        return Ackermann(m - 1, 1);
+    }
+    else
+    {
+        return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
+}
+
+
